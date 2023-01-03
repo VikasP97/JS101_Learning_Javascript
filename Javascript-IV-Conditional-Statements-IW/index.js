9a54c2b87dcceea5b237a0e7c26458e8b29bad67
vikasbhu <vikasbhu1997@gmail.com>
2023-01-03 10:00:22 +0530

    add sencond assignment

---
+++ Javascript-IV-Conditional-Statements-IW/index.js
@@ -0,0 +1,13 @@
+//Problem 4: Given stored username and password and input username and password, Print if the user can login or not.
+
+let userName="Vikasbhu1997@gmail.com";
+let password="12345";
+
+let typeUser="bhuvikas@gmail.com";
+let typePass="54321";
+
+if(userName==typeUser && password==typePass){
+  console.log("login")
+}else{
+  console.log("not");
+}
